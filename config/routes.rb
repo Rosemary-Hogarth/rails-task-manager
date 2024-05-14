9c81bab2c7278572c6f210a723c6d093e62b4763
Rosemary Hogarth <rohogarth@gmail.com>
2024-05-14 09:13:23 +0200

    Functionality done - styling ToDo

--- config/routes.rb
+++ config/routes.rb
@@ -7,14 +7,14 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
-  get 'tasks/new', to: 'tasks#new'
-  get '/tasks/:id', to: 'tasks#show', as: :task
-  get '/tasks', to: 'tasks#index'
-  post 'tasks', to: 'tasks#create'
-  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit
-  patch 'tasks/:id', to: 'tasks#update'
-  delete 'tasks/:id', to: 'tasks#destroy'
+  # get 'tasks/new', to: 'tasks#new'
+  # get '/tasks/:id', to: 'tasks#show', as: :task
+  # get '/tasks', to: 'tasks#index'
+  # post 'tasks', to: 'tasks#create'
+  # get 'tasks/:id/edit', to: 'tasks#edit', as: :edit
+  # patch 'tasks/:id', to: 'tasks#update'
+  # delete 'tasks/:id', to: 'tasks#destroy'
 
-  # resources :tasks
+  resources :tasks
   # root 'tasks#index'
 end
